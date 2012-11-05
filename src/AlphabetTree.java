1a1221948ccdc6db070c5d80e872b18cfe05a572
Michael <Michael@Michael-PC>
2012-11-04 17:46:32 -0800

    Improved comments and code clarity

--- src/AlphabetTree.java
+++ src/AlphabetTree.java
@@ -1,21 +1,28 @@
 /**
  * An AlphabetTree to store words that will later be used for text parsing. 
  * Each level has a branching factor of 26 possible letters
+ * The only values stored in the tree are at the leaf nodes, since the
+ * "letter pointers" are implicit (a-z map to 0-25)
+ * 
  * @author Michael Riha
  */
 public class AlphabetTree 
 {
     private Node root;
     
+    /**
+     * A node in the tree which has up to 26 children and a weighted value
+     * If the weight is 0, then the node is not a leaf on the tree
+     */
     private class Node
     {
-        private double weight; // non-zero weight means a leaf node
-        private Node[] pointers;
+        private double weight;
+        private Node[] children;
         
         private Node()
         {
             weight = 0.0;
-            pointers = new Node[26];
+            children = new Node[26];
         }
     }
     
@@ -42,23 +49,24 @@ public class AlphabetTree
         {
             next_idx = (int) (word.charAt(i) - 'a');
             
-            if (cur.pointers[next_idx] == null) // add node that wasn't there
-                cur.pointers[next_idx] = new Node();
+            if (cur.children[next_idx] == null) // add node that wasn't there
+                cur.children[next_idx] = new Node();
             
-            cur = cur.pointers[next_idx]; // descend the tree
+            cur = cur.children[next_idx]; // descend the tree
         }
         cur.weight = weight; // reached a leaf so add the weight
     }
     
     /**
-     * Get the weight of a word in the tree, if it exists
+     * Get the weight of a word in the tree, if it exists, including wildcards
+     * E.g. if "critic" is in the tree, "critically" will map to that
      * @param word the word to look for
      * @return the weight of the word parameter
      */
     public double getWeight(String word)
     {
         Node cur = root;
-        Node next_ptr;
+        Node next;
         word = word.toLowerCase();
         
         for (int i = 0; i < word.length(); ++i)
@@ -66,12 +74,12 @@ public class AlphabetTree
             if (cur.weight > 0.1) // "wildcard" found
                 return cur.weight;
             
-            next_ptr = cur.pointers[(int) word.charAt(i) - 'a'];
+            next = cur.children[(int) word.charAt(i) - 'a'];
             
-            if (next_ptr == null) // word not in tree
+            if (next == null) // word not in tree
                 return 0.0;
             
-            cur = next_ptr; // descend the tree
+            cur = next; // descend the tree
         }
         return cur.weight;
     }   
