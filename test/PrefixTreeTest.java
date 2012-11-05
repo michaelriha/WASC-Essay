29a87df3f2ce8d960f22bb1a60dbcebaec3c0cf0
Michael <Michael@Michael-PC>
2012-11-04 18:47:43 -0800

    renamed to PrefixTree

---
+++ test/PrefixTreeTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Michael
+ */
+public class PrefixTreeTest {
+    
+    public PrefixTreeTest() 
+    {
+    }
+
+    /**
+     * Test of addWord and getWeight methods in PrefixTree class
+     */
+    @Test
+    public void testAddWord() {
+        System.out.println("addWord");
+        
+        String[] words = new String[]{"This", "Critical", "Task", "Is", "The",
+            "Necessary", "Thing", "That", "We",  "Must", "Do", "Zzz", "Zzyzyx"
+        };
+
+        Double[] weights = new Double[]{1.0, 2.7, 3.4, 1.6, 1.1, 4.0, 3.2, 1.1,
+            3.0, 3.7, 3.8, 1.0, 4.0
+        };
+        
+        PrefixTree instance = new PrefixTree();
+        for (int i = 0; i < words.length; ++i)
+        {            
+            instance.addWord(words[i], weights[i]);
+        }
+        
+        for (int i = 0; i < words.length; ++i)
+        {
+            assertEquals(weights[i], instance.getWeight(words[i]), 0.01);
+        }
+        
+        assertEquals(0.0, instance.getWeight("Tas"), 0.01);
+        assertEquals(0.0, instance.getWeight("Notintree"), 0.01);
+        assertEquals(2.7, instance.getWeight("Critically"), 0.01);
+    }
+}
