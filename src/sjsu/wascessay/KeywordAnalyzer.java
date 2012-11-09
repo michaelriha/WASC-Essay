6a7d85483a8c7c097a66400b22754c77c196d1f3
Michael <Michael@Michael-PC>
2012-11-08 16:35:40 -0800

    refactored several functions

--- src/sjsu/wascessay/KeywordAnalyzer.java
+++ src/sjsu/wascessay/KeywordAnalyzer.java
@@ -25,21 +25,35 @@ public class KeywordAnalyzer
     private static final int RUBRICS = 5, WEIGHTS = 2;
     private static final int a = 6, b = 1, c = 4;
     
-    /**
-     * Reads keywords.txt in CWD and builds a prefix tree from it. The expected 
-     * formatting for each line, one entry per line, is: keyword,weight,rubric
-     * CSV values with no spacing. E.g. Critical,2,1
-     */
-    public KeywordAnalyzer() throws FileNotFoundException, IOException
+    /** Constructs a KeywordAnalyzer with no statistical data */
+    public KeywordAnalyzer()
     {
         keywordsUsed = new SortedSet[RUBRICS][WEIGHTS];
         wordCounts = new int[RUBRICS][WEIGHTS];
         scores = new double[RUBRICS+1];
-        totalWords = 0;
-        
-        // build prefix tree from keywords.txt
         keywordTree = new PrefixTree();
-        BufferedReader br = new BufferedReader(new FileReader("keywords.txt"));
+        totalWords = 0;
+    }
+    
+    /** Resets instance fields and keyword occurrences in the tree */
+    public void resetFields()
+    {        
+        keywordsUsed = new SortedSet[RUBRICS][WEIGHTS];
+        wordCounts = new int[RUBRICS][WEIGHTS];
+        scores = new double[RUBRICS+1];
+        keywordTree.reset();
+        totalWords = 0;
+    }
+    
+    /** 
+     * Adds the keywords from the specified file to the keyword tree
+     * Each line should contain one entry in the format: keyword,weight,rubric
+     * Example: Critical,2,1 would create an entry with weight 2 for rubric 1
+     * @param filename the file to read
+     */
+    public void readKeywordFile(String filename) throws FileNotFoundException, IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
         try 
         {
             String[] line = br.readLine().split(",");
@@ -56,26 +70,15 @@ public class KeywordAnalyzer
     }
     
     /**
-     * This function parses an arbitrary amount of text stored as 
-     * ArrayList<String>. As it checks for each word in the tree, it stores the
-     * number of times each keyword has appeared and how many total words there
-     * are for later use. These word occurrence counts are cleared every time 
-     * this function is called.Then, based on keyword statistics collected it 
-     * assigns a score for each rubric. 
-     * 
-     * @param words the text to analyze
-     * @return an array of scores between 0.0 and ?.0. Index 5 contains the
-     * total score, and 0-4 contain those pertaining to the 5 proficiencies
-     * in the order given
+     * Parses each word in the input and updates the keyword statistics fields
+     * @param words Plaintext alphabetic non-numeric words. one word per entry
      */
-    public double[] calculateScores(ArrayList<String> words)
+    public void parseText(ArrayList<String> words)
     {
-        // initialize and clear any leftover occurrence fields
         Node values;
         int rubric, weight;
         keywordTree.reset();
         
-        // parse the input and collect wordcount statistics for calculation
         for (String word : words)
         {
             values = keywordTree.find(word);
@@ -87,15 +90,23 @@ public class KeywordAnalyzer
                 keywordsUsed[rubric - 1][weight - 1].add(word);
             }
             ++totalWords;
-        }         
-        // calculate the scores, average the sum of the scores for the total
+        }
+    }
+    
+    /**
+     * Calculates each rubric score and returns them in order with the total 
+     * rubric score (simple average) at the end
+     * @return an array of scores between 0.0 and 4.0. [r1, r2, r3, r4, r5, Tot]
+     */
+    public double[] calculateScores()
+    {
         double score, sum = 0.0;
         for (int i = 0; i < RUBRICS; ++i)
         {
             score = calculateScore(wordCounts[i][0], wordCounts[i][1], 
                                                                     totalWords);
             scores[i] = score;
-            sum += score;            
+            sum += score;
         }
         scores[RUBRICS] = sum / RUBRICS;
         return scores;
@@ -103,42 +114,27 @@ public class KeywordAnalyzer
     
     /**
      * Calculates an individual rubric score based on the number of weight one
-     * and two words as well as the total number of words in the document
-     * 
+     * and two words as well as the total number of words in the document 
      * @param weightOneCount the number of weight one words
      * @param weightTwoCount the number of weight two words
      * @param totalWordCount the total word count
-     * @return a rubric score between 0.0 and ?.0 based on the formula
-     * score= (N/a)*((1+d1)^b)*((1+dr)^c)
+     * @return a rubric score between 0.0 and 4.0 based on the formula
+     * score = (N/a)*((1+d1)^b)*((1+dr)^c)
      * N: Total number of keywords
      * d1: Density of weight one words (weightOneCount / N)
-     * d2: Density of weight two words (weightTwoCount / N)
      * dr: Density of related words (N / totalWordCount)
      */
-    public double calculateScore(int weightOneCount, int weightTwoCount,
+    public static double calculateScore(int weightOneCount, int weightTwoCount,
                                         int totalWordCount)
     {
-        if (totalWordCount <= 0) 
-            return 0.0;
+        if (totalWordCount <= 0) return 0.0;
         int N = weightOneCount + weightTwoCount;
         double dr = N / totalWordCount;
-        double d1; //, d2;
-        if (N == 0)
-        {
-            d1 = 0;
-            //d2 = 0;
-        }
-        else
-        {
-            d1 = weightOneCount / N;
-            //d2 = weightTwoCount / N;
-        }        
-        return Math.min(1, (N / a) * Math.pow(1 + d1, b) * Math.pow(1 + dr, c));
+        double d1 = (N == 0) ? 0 : weightOneCount / N;
+        return Math.min(4, 4*((N/a) * Math.pow(1+d1, b) * Math.pow(1+dr, c)));
     }
     
-   /**
-    * Gets how many times the keyword was found in the text
-    * @param keyword the word to look for
+   /**@param keyword the word to look for
     * @return the number of times the keyword was found
     */
     public int getKeywordOccurrences(String keyword)
@@ -146,13 +142,10 @@ public class KeywordAnalyzer
         return keywordTree.findNoIncrement(keyword).getOccurrences();
     }
     
-    /**
-     * Returns the keywords used for each rubric and weight
-     * @return 2 dimensional array of SortedStrings containing the keywords for
-     * keywordsUsed[rubric][weight]
-     */
-    public SortedSet<String>[][] getKeywordsUsed()
-    {
-        return keywordsUsed;
-    }
+    /**@return 2 dimensional array of SortedStrings containing the keywords for
+     * keywordsUsed[rubric][weight] */
+    public SortedSet<String>[][] getKeywordsUsed() { return keywordsUsed; }
+    
+    /**@return Scores in this format [rub1, rub2, rub3, rub4, rub5, total] */
+    public double[] getScores() { return scores; };
 }
